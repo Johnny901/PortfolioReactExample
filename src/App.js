8f3e5ff53660c105fe89ce83acc8612ffd2f5271
Johnny King <johnny@xpand.group>
2020-07-01 11:54:08 +0100

    HTML refactored to React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+//import Content from "./components/Content";
+import Header from "./components/Content";
+import About from "./components/Content";
+import Portfolio from "./components/Content";
+import Contact from "./components/Content";
+import Footer from "./components/Content";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <body class="w3-black">
+          <Nav></Nav>
+          <div class="w3-padding-large" id="main">
+            <Header></Header>
+            <About></About>
+            <Portfolio></Portfolio>
+            <Contact></Contact>
+            <Footer></Footer>
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class Portfolio extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="w3-padding-64 w3-content" id="photos">
+        <h2 className="w3-text-light-grey">My Photos</h2>
+        <hr style={{ width: "200px" }} className="w3-opacity" />
+        {/* Grid for photos */}
+        <div className="w3-row-padding" style={{ margin: "0 -16px" }}>
+          <div className="w3-half">
+            <img src="/w3images/wedding.jpg" style={{ width: "100%" }} />
+            <img src="/w3images/rocks.jpg" style={{ width: "100%" }} />
+            <img src="/w3images/sailboat.jpg" style={{ width: "100%" }} />
+          </div>
+          <div className="w3-half">
+            <img src="/w3images/underwater.jpg" style={{ width: "100%" }} />
+            <img src="/w3images/chef.jpg" style={{ width: "100%" }} />
+            <img src="/w3images/wedding.jpg" style={{ width: "100%" }} />
+            <img src="/w3images/p6.jpg" style={{ width: "100%" }} />
+          </div>
+          {/* End photo grid */}
+        </div>
+        {/* End Portfolio Section */}
+      </div>
+    );
+  }
+}
