a1b0b1b614dafde8e737fe0308ff814ea5227449
Johnny <johnny@xpand.group>
2020-07-02 10:10:27 +0100

    Updates

---
+++ precache-manifest.e9fb63d1aba54f5860830686c262dd15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c711ddd1faeb7cfaeb6cb38f5e39711a",
+    "url": "/PortfolioReactExample/index.html"
+  },
+  {
+    "revision": "9b3fcfc43453e3c7480b",
+    "url": "/PortfolioReactExample/static/css/main.a659e38c.chunk.css"
+  },
+  {
+    "revision": "222721d8182aea7c42d5",
+    "url": "/PortfolioReactExample/static/js/2.9f568fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortfolioReactExample/static/js/2.9f568fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3fcfc43453e3c7480b",
+    "url": "/PortfolioReactExample/static/js/main.f1bf7c32.chunk.js"
+  },
+  {
+    "revision": "55f9189c47ddcd09d453",
+    "url": "/PortfolioReactExample/static/js/runtime-main.bb4572e6.js"
+  }
+]);
